c86339e4744f6274461a1aceb25198596d7ece47
kalyanirishabh <54349200+kalyanirishabh@users.noreply.github.com>
2019-10-16 12:39:43 +0530

    Update extract_faces.py

--- extract_faces.py
+++ extract_faces.py
@@ -1,3 +1,31 @@
+import numpy as np
+import cv2
+import tensorflow as tf
+import time
+import sys
+import os
+import shutil
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import keras.backend as K
+from keras.layers import Lambda,Dense,Flatten,Conv2D,MaxPool2D,Input,Embedding
+from keras.preprocessing.image import ImageDataGenerator,array_to_img,img_to_array,load_img
+from keras.utils import np_utils
+from keras.models import load_model
+from keras.optimizers import RMSprop,SGD
+from keras.models import Sequential,Model
+from imutils import paths
+from PIL import Image
+from numpy import asarray,savez_compressed,load,expand_dims
+from mtcnn.mtcnn import MTCNN
+from vidgear.gears import VideoGear
+from os import listdir
+from os.path import isdir
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder,Normalizer
+from sklearn.svm import SVC
+from pathlib import Path
+
 def extract_face(path,image,required_size=(160, 160)):
     """
       This function extract multiple faces from a given image or path of the image using MTCNN 
